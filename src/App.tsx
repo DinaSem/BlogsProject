9fc1df4cd679ad89a8978d39a2cd192119dc30a1
dell <neispravimayas1@gmail.com>
2022-11-29 22:08:59 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import s from './App.module.css'
+import {Route, Routes} from "react-router-dom";
+import BlogsPage from "./blogs/BlogsPage";
+import {PostsPage} from "./posts/PostsPage";
+import Navbar from "./navbar/Navbar";
+import Header from "./header/Header";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import {Blog} from "./blogs/blog/Blog";
+import {BlogDetails} from "./blogs/blog/blogDetails/BlogDetails";
+import {BreakfastDining} from "@mui/icons-material";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+
+        <Box className={s.appWrapper}>
+            <Header/>
+            <Container style={{maxWidth: '100%', position: "relative"}}>
+                <Breadcrumbs/>
+                <Box sx={{display: 'flex'}} style={{backgroundColor: '#FCFBFB', position: "relative"}}>
+                    <Navbar/>
+                    <Box component="main" sx={{flexGrow: 1, p: 3,}}
+                         style={{backgroundColor: '#f6f2f2', margin: '-24px', marginTop: '30px', width: '100%'}}>
+                        <Routes>
+                            <Route path='/' element={<BlogsPage/>}/>
+                            <Route path='posts' element={<PostsPage/>}/>
+                            <Route path='blog' element={<BlogDetails/>}/>
+                        </Routes>
+                    </Box>
+                </Box>
+            </Container>
+        </Box>
+
+    );
 }
-
-export default App;
---
+++ src/App1.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import s from './App1.module.css'
+import {Route, Routes} from "react-router-dom";
+import BlogsPage from "./blogs/BlogsPage";
+import {PostsPage} from "./posts/PostsPage";
+import Header from "./header/Header";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import {BlogDetails} from "./blogs/blog/blogDetails/BlogDetails";
+import Navbar from "./navbar/Navbar";
+import {PostDetails} from "./posts/post/postDetails/PostDetails";
+
+
+export default function App1() {
+
+    return (
+
+        <Container className={s.appWrapper} style={{
+            padding: "0"
+        }}>
+            <Header/>
+            <Box style={{
+                display: "flex",
+                justifyContent: "center",
+                minHeight: '100%',
+                boxSizing: "border-box",
+                maxWidth: '100%',
+                padding: '63px 0 0 0 ',
+                backgroundColor: '#f6f2f2'
+            }}>
+
+                <Box style={{
+                    flex: '1 0 auto',
+                    minHeight: '100%',
+                    boxSizing: "border-box",
+                    color: "black",
+                    maxWidth: '20%',
+                    backgroundClip: 'content-box',
+                    position: "relative",
+                    backgroundColor: '#FAF7F8',
+                }}>
+                    <Navbar/>
+                </Box>
+                <Box style={{
+                    flex: '5 0 auto',
+                    boxSizing: "border-box",
+                    maxWidth: '80%',
+                    backgroundClip: 'content-box',
+                }}>
+                    <Box component="main"
+                         style={{
+                             backgroundColor: '#f6f2f2',
+                             width: '100%'
+                         }}>
+                        <Routes>
+                            <Route path='/' element={<BlogsPage/>}/>
+                            <Route path='/posts/' element={<PostsPage/>}/>
+                            <Route path='blog/:id' element={<BlogDetails/>}/>
+                            <Route path='posts/post/:id' element={<PostDetails/>}/>
+                        </Routes>
+                    </Box>
+                </Box>
+            </Box>
+        </Container>
+
+    );
+}
