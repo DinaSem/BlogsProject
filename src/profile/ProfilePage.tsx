b78348f582dcd4ded7b178647d3512243d42dcd2
dell <neispravimayas1@gmail.com>
2022-12-28 00:23:52 +0300

    style devices

--- src/profile/ProfilePage.tsx
+++ src/profile/ProfilePage.tsx
@@ -5,23 +5,27 @@ import Box from "@mui/material/Box";
 import {useAppDispatch, useAppSelector} from "../hooks";
 import {fetchProfileDataTC} from "./profile-reducer";
 import {Button} from "@mui/material";
-import {isExpired, useJwt} from "react-jwt";
 import {decodeToken} from "react-jwt";
 import {Navigate} from "react-router-dom";
-import {parseUserAgent, useDeviceSelectors} from 'react-device-detect';
+import {parseUserAgent} from 'react-device-detect';
+import chrome from'../pictures/chrome.png'
+import edge from'../pictures/edge.png'
 
-type parsedDate ={
-
-
-
-}
+// const icon = getIcon(browser);
+// function getIcon(browser:string) {
+//     if (browser === "Chrome") {
+//         return chrome;
+//     } else if (browser === "Edge") {
+//         return edge;
+//     }
+//     return icon0;
+// }
 
 export const ProfilePage = () => {
     const dispatch = useAppDispatch()
     const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
     const profileData = useAppSelector(state => state.profile.profileData)
     const token = localStorage.getItem('jwt_token');
-
 
 //массив девайсов (авторизаций), по девайс айди находишь 1н(в массиве-токене) это и есть активная ссесия
 
@@ -31,21 +35,16 @@ export const ProfilePage = () => {
     //нахожу девайс, на котором открыт браузер
     const infoOfThisDevice = profileData.find(d => d.deviceId === myDecodedToken?.deviceId)
     //другие девайсы, с которых был осуществлен вход
-    // const allOtherDevices = profileData.find(d => d.deviceId !== myDecodedToken?.deviceId)
     const allOtherDevices = profileData.filter(d => d.deviceId !== myDecodedToken?.deviceId)
 
-    console.log('profileData', profileData)
-    console.log('myDecodedToken', myDecodedToken)
-    console.log('infoOfThisDevice', infoOfThisDevice)
-
-
-    //const userAgent = window.navigator.userAgent
-
-
+//TODO решить вопрос с типизацией
     // @ts-ignore
     const userDeviceInfo = parseUserAgent(infoOfThisDevice)
-    console.log('parsedMyDataDevice',userDeviceInfo)
+    // @ts-ignore
+    const allDev = allOtherDevices.map(d=>parseUserAgent(d.title))
 
+    console.log('allOtherDevices',allOtherDevices)
+    console.log('allDev',allDev)
 
 
     useEffect(() => {
@@ -76,12 +75,18 @@ export const ProfilePage = () => {
                     height: "120px",
                     boxShadow: '0px 5px 20px rgba(29, 33, 38, 0.03), 0px 1px 2px rgba(29, 33, 38, 0.1)',
                     borderRadius: '2px',
-                    padding: '17px'
+                    padding: '17px',
+                    display:"flex",
                 }}>
-                    {/*<h4>{userDeviceInfo?.title}</h4>*/}
-                    <h4>{userDeviceInfo?.browser.name}</h4>
-                    <p>IP: {infoOfThisDevice?.ip}</p>
-                    <h4 style={{color: '#3677F7'}}>Online</h4>
+                    <Box style={{margin:'20px 10px'}}>
+                        <img src={
+                            userDeviceInfo?.browser.name === 'Chrome'? chrome:edge
+                        } alt="" style={{width:'36px'}}/>
+                    </Box>
+                    <Box>{/*<h4>{userDeviceInfo?.title}</h4>*/}
+                        <h4>{userDeviceInfo?.browser.name}</h4>
+                        <p>IP: {infoOfThisDevice?.ip}</p>
+                        <h4 style={{color: '#3677F7'}}>Online</h4></Box>
                 </Box>
                 <Box style={{display: "flex", justifyContent: 'end'}}>
                     <Button style={{
@@ -94,6 +99,7 @@ export const ProfilePage = () => {
                         fontWeight: '600', lineHeight: ' 20px',
                         textTransform: 'none',
                         margin: '20px 0',
+
                     }}>
                         Terminate all other session
                     </Button>
@@ -113,11 +119,16 @@ export const ProfilePage = () => {
                                 height: "120px",
                                 boxShadow: '0px 5px 20px rgba(29, 33, 38, 0.03), 0px 1px 2px rgba(29, 33, 38, 0.1)',
                                 borderRadius: '2px',
-                                padding: '17px'
-                            }} key={i}>
-                                <h4>{parseUserAgent(d)?.os.name} {parseUserAgent(d)?.os.version}</h4>
-                                <p>IP: {d?.ip}</p>
-                                <p>Last visit:{day + '.' + month + '.' + year}</p>
+                                padding: '17px',
+                                display:"flex",
+                            }}key={i}>
+                                <Box style={{margin:'20px 10px'}}>
+                                    <img src={parseUserAgent(d.title)?.browser.name === 'Chrome'? chrome:edge} alt="" style={{width:'36px'}}/>
+                                </Box>
+                                <Box><h4>{parseUserAgent(d.title)?.browser.name}</h4>
+                                    <p>IP: {d?.ip}</p>
+                                    <p>Last visit:{day + '.' + month + '.' + year}</p>
+                                </Box>
                             </Box>
                             )
                     })}
